10c6957afd1420233b3aeb8399afe805c7d048e2
rpiston <heidozanzo@live.com>
2022-02-21 15:04:48 -0300

    AHORA SI NENAAAAAA

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -6,91 +6,60 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private Camera cameraPlayer;
-	
-    [SerializeField] private float movementSpeed;
-    
+    [SerializeField] public float speed = 6f;
+    [SerializeField] float angleRotation = 45;
+    [SerializeField] float turnSmoothTime = 0.1f;
+    private CharacterController crPlayer;
+    private Vector3 target;
+    private Vector3 moveDirection;
 	private Animator animator;
- 	private Rigidbody rb;
-    private Vector3 movementVector;
-	private Vector3 targetVector;
-	private Vector3 target;
-	private bool isRunning;
-	private Ray ray;
-    private float mouseX;
-    private float mouseY;
-    Vector2 mousePosFromCenter; 
-
-	void Start()
-	{
+    private Ray ray;
+    private float gravity = 9.85f;
+    private float velocityZ;
+    private float velocityX;
+    void Start(){
+        crPlayer = GetComponent<CharacterController>();
 		animator = GetComponent<Animator>();
-		rb = GetComponent<Rigidbody>(); 
-	}
-	void Update()
-    {
-        mouseX = Input.mousePosition.x;
-        mouseY = Input.mousePosition.y;
-
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical"); 
-
-        targetVector = new Vector3(horizontalInput, 0, verticalInput); 
-        targetVector.Normalize();
-
-        movementVector = MoveTowardTarget(targetVector);
-
-        if (targetVector != Vector3.zero)
-        {
-            transform.forward = movementVector;
-        }
-        RotateFromMouseVector();
-
-        CheckAnimatorState();
     }
 
-    private void CheckAnimatorState()
+    void Update()
     {
-        mousePosFromCenter = new Vector2(mouseX - Screen.width/2, Screen.height/2 - mouseY).normalized;
-        
-        Debug.Log($"mousePosFromCenter:{mousePosFromCenter}");
-        if (Input.GetKey(KeyCode.Mouse0))
-        {
-            if ((Input.GetKey(KeyCode.S) && mousePosFromCenter.y < 0) || (Input.GetKey(KeyCode.W) && mousePosFromCenter.y > 0)
-                || (Input.GetKey(KeyCode.D) && mousePosFromCenter.x < 0) || (Input.GetKey(KeyCode.A) && mousePosFromCenter.x > 0)
-            )
-            {
-                animator.SetBool("isRunningBackwards", true);
-            }
-            else
-            {
-                animator.SetBool("isRunningBackwards", false);
-            }
-            ShootAnim(1,true);
-        }
-        else
-        {
-            ShootAnim(0,false);
-            animator.SetBool("isRunningBackwards", false); 
-        }
+        Move();
+        Rotate();
+        ApplyGravity();
+        Animate();
+    }
 
-        if(!animator.GetBool("isRunningBackwards")){
-            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S))
-            {
-                animator.SetBool("isRunning", true); 
-            }
-            else
-            {
-                animator.SetBool("isRunning", false);
-            }
+    private void ApplyGravity()
+    {
+        // Apply gravity
+        moveDirection.y -= gravity * Time.deltaTime;
+    }
+
+    private void Animate()
+    {
+        velocityZ = Vector3.Dot(moveDirection.normalized, transform.forward);
+        velocityX = Vector3.Dot(moveDirection.normalized, transform.right);
+
+        animator.SetFloat("VelocityZ", velocityZ);
+        animator.SetFloat("VelocityX", velocityX);
+    }
+
+    private void Move()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        moveDirection = new Vector3(horizontal, 0f, vertical).normalized;
+        if (moveDirection.magnitude >= 0.1f)
+        {
+            // -- Se corrije la dirección del player con la rotación de la camara para que respete el horizontal/vertical
+            moveDirection = Quaternion.Euler(0, cameraPlayer.gameObject.transform.eulerAngles.y, 0) * moveDirection;
+            moveDirection.Normalize();
+            crPlayer.Move(moveDirection * speed * Time.deltaTime);
         }
     }
 
-    private void ShootAnim(int weight,bool value)
-    {
-        animator.SetLayerWeight(animator.GetLayerIndex("UpperBodyMovement"), weight);
-        animator.SetBool("isShotting", value);
-    }
-
-    private void RotateFromMouseVector()
+    private void Rotate()
     {
         ray = cameraPlayer.ScreenPointToRay(Input.mousePosition);
 
@@ -101,17 +70,4 @@ public class PlayerMovement : MonoBehaviour
             transform.LookAt(target);
         }
     }
-
-    private Vector3 MoveTowardTarget(Vector3 targetVector)
-    {
-        isRunning = true;
-        var speed = movementSpeed * Time.deltaTime;
-        targetVector = Quaternion.Euler(0, cameraPlayer.gameObject.transform.rotation.eulerAngles.y, 0) * targetVector;
-		var targetPosition = transform.position + targetVector * speed;
-        transform.position = targetPosition;
-
-        return targetVector;
-    }
-
-
 }
